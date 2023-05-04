3e865a5744a94a4ee11117e8c007b55fb0718121
Tatiane Rodrigues <93277324+tatianerodriguess@users.noreply.github.com>
2023-05-03 21:07:03 -0300

    Operações

---
+++ teste02.js
@@ -0,0 +1,47 @@
+/* OPERADORES MATEMÁTICOS */
+
+"use strict"
+
+let num1= 0, num2= 0, res= 0
+
+num1= 5
+num2= 10
+
+res = (num1 + num2) * 3
+
+console.log(res)
+
+// usando o modulo para me dar o resto da divisão
+
+let num3= 0, num4= 0, res2= 0
+
+num3= 17
+num4= 2
+
+res2 = num3%num4
+
+console.log(res2)
+console.log(num3/num4)
+
+// OPERADORES DE INCREMENTO e DECREMENTO
+
+let num5= 0
+num5= 10
+num5++
+num5++
+num5++
+num5++
+num5--
+num5--
+
+num5+=1 // é o mesmo que num5++ e num5= num5+1
+
+num5+=4  /* posso usar qualquer valor pra somar na variavel
+vale para qualquer operação matemática */
+
+console.log(num5)
+
+
+
+
+
